c608f2d5bfe3d6a451c1cf6aba07fca3f1257334
Arseniy <austjuzhaninov@landau-inc.ru>
2022-05-31 18:50:38 +0500

    example: data binding

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -6,10 +6,12 @@ use Livewire\Component;
 
 class HelloWorld extends Component
 {
+    public string $name = 'Ivan';
+    public bool $loud = false;
+    public array $greeting = ['Hello'];
+
     public function render()
     {
-        return view('livewire.hello-world',
-            ['name' => 'Ivan'] // example of passing data into template
-        );
+        return view('livewire.hello-world');
     }
 }
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,15 @@
 <div>
-    Hello {{ $name }}
+    <input wire:model="name" type="text">
+    {{--wire:model.debounce.10000ms="name" - delay after up key--}}
+    {{--wire:model.lazy="name" -request sent after click away--}}
+
+    <input wire:model="loud" type="checkbox">
+    <select wire:model="greeting" multiple>
+        <option>Hello</option>
+        <option>Goodbye</option>
+        <option>Adios</option>
+    </select>
+
+    {{--Hello {{ $loud ? strtoupper($name) : $name }}--}}
+    {{ implode(', ', $greeting) }} {{ $name }} @if($loud) ! @endif
 </div>
